ccae7330ce3bfab84a3855083d33cc307ab41567
wfing <757001119@qq.com>
2015-10-22 11:26:12 +0800

    3d plot

--- plot_dbscan.py
+++ plot_dbscan.py
@@ -26,7 +26,7 @@ import matplotlib.pyplot as plt
 
 
 def dbscan(EPS,MIN_SAMPLE):
-    XX=np.loadtxt(".\\GPS_Get_PreProcesser\\7-11-2015\\locationGPS.txt",dtype=float,delimiter=',',skiprows=1,usecols=(0,1),unpack=False)
+    XX=np.loadtxt(".\\GPS_Get_PreProcesser\\10-20-2015\\locationGPS.txt",dtype=float,delimiter=',',skiprows=1,usecols=(0,1),unpack=False)
     #Latitude,Lat,XX=GPS_Kalman_Filter.Get_Prime_GpsData(".\\GPS_Get_PreProcesser\\7-11-2015\\locationGPS.txt")
     centers = [[1, 1], [-1, -1], [1, -1]]
     db = DBSCAN(eps=EPS, min_samples=MIN_SAMPLE).fit(XX)
@@ -43,7 +43,9 @@ def dbscan(EPS,MIN_SAMPLE):
 
     # Black removed and is used for noise instead.
     fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
+    ax = fig.add_subplot(211, projection='3d')
+    #ax = fig.add_plot( projection='3d')
+
     unique_labels = set(labels)
     colors = plt.cm.Spectral(np.linspace(0, 1, len(unique_labels)))
     for k, col in zip(unique_labels, colors):
@@ -53,14 +55,64 @@ def dbscan(EPS,MIN_SAMPLE):
 
         class_member_mask = (labels == k)
 
-        xy = XX[class_member_mask & core_samples_mask]
+        xx = XX[class_member_mask & core_samples_mask]
 
         #ax.scatter(xy[:, 0], xy[:, 1],np.array([1]*len(xy[:, 0])), 'o', markerfacecolor=col,
              #markeredgecolor='k', markersize=8)
-        ax.scatter(xy[:, 0], xy[:, 1],np.array([1]*len(xy[:, 0])), c=col)
+        ax.scatter(xx[:, 0], xx[:, 1],np.array([1]*len(xx[:, 0])), c=col)
         #xy = XX[class_member_mask & ~core_samples_mask]
         #plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=col,
          #    markeredgecolor='k', markersize=4)
+
+    #--------------------------------------------------------------------------------------------------------------------------------------------#
+        #plot 2chen
+    YY=np.loadtxt(".\\GPS_Get_PreProcesser\\7-12-2015\\locationGPS.txt",dtype=float,delimiter=',',skiprows=1,usecols=(0,1),unpack=False)
+    #Latitude,Lat,XX=GPS_Kalman_Filter.Get_Prime_GpsData(".\\GPS_Get_PreProcesser\\7-11-2015\\locationGPS.txt")
+    centers = [[1, 1], [-1, -1], [1, -1]]
+    db = DBSCAN(eps=EPS, min_samples=MIN_SAMPLE).fit(YY)
+    #db = DBSCAN(eps=0.002, min_samples=10).fit(XX)  3
+    #db = DBSCAN(eps=0.0032, min_samples=10).fit(XX)   2
+    core_samples_mask = np.zeros_like(db.labels_, dtype=bool)
+    core_samples_mask[db.core_sample_indices_] = True
+    labels = db.labels_
+    # Number of clusters in labels, ignoring noise if present.
+    n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
+
+    ##############################################################################
+    # Plot result
+
+    # Black removed and is used for noise instead.
+
+    ax = fig.add_subplot(212, projection='3d')
+    unique_labels = set(labels)
+    colors = plt.cm.Spectral(np.linspace(0, 1, len(unique_labels)))
+    for k, col in zip(unique_labels, colors):
+        if k == -1:
+            # Black used for noise.
+            col = 'k'
+
+        class_member_mask = (labels == k)
+
+        yy = YY[class_member_mask & core_samples_mask]
+
+        #ax.scatter(xy[:, 0], xy[:, 1],np.array([1]*len(xy[:, 0])), 'o', markerfacecolor=col,
+             #markeredgecolor='k', markersize=8)
+        ax.scatter(yy[:, 0], yy[:, 1],np.array([2]*len(yy[:, 0])), c=col)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     plt.title('Estimated number of clusters: %d' % n_clusters_)
     plt.show()
     '''
