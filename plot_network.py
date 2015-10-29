22c8bcb2836a6073fcb00339c3ce9b7c07853613
wfing <757001119@qq.com>
2015-10-29 20:42:06 +0800

    dtw simlar

--- plot_network.py
+++ plot_network.py
@@ -5,22 +5,23 @@ import math
 import numpy as np
 import networkx as nx
 from decimal import *
-data = np.loadtxt('network.txt',dtype=str,delimiter=' ',usecols=(0,1,2))
+data = np.loadtxt('network.txt',dtype=str,delimiter=',',usecols=(0,1,2))
 
 print (data)
 G = nx.Graph()
 for i in range(0,len(data)):
-    if float(data[i][1])!=0.0:
-        G.add_node(i+1)
+    if float(data[i][2])!=0.0:
+        G.add_node(int(data[i][0]))
+        G.add_node(int(data[i][1]))
 
 for i in range(0,len(data)):
-        G.add_edge(int(data[i][1]),int(data[i][2]),weight=float((data[i][0])))
+        G.add_edge(int(data[i][0]),int(data[i][1]),weight=float((data[i][2])))
 
 pos=nx.spring_layout(G,iterations=20)
 edgewidth=[]
 for (u,v,d) in G.edges(data=True):
     #print G.get_edge_data(u,v).values()
-    edgewidth.append(round(G.get_edge_data(u,v).values()[0]*500,2))
+    edgewidth.append(round(G.get_edge_data(u,v).values()[0]*160,2))
 
 nx.draw_networkx_edges(G,pos,width=edgewidth,with_labels=True)
 #nx.draw_networkx_nodes(G,pos,with_labels=True)
