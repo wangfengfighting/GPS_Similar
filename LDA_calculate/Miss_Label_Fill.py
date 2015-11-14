a73cb9474fec912c55e0b0642795d9402c6ef836
wfing <757001119@qq.com>
2015-11-14 13:54:33 +0800

    label 缺失的添加

--- LDA_calculate/Miss_Label_Fill.py
+++ LDA_calculate/Miss_Label_Fill.py
@@ -13,19 +13,39 @@ def  Read_RC_stoppoint(path):
 
                        #converters={3:lambda s:datetime.datetime.strptime(s,'%Y-%m-%d %H:%M:%S')},
 
-    print RC_stop
+    #print RC_stop
     RCedSP=[]
-    temp=[]
-    print str2time(RC_stop[199][3],RC_stop[0][3])
+
+    #print str2time(RC_stop[199][3],RC_stop[0][3])
     index=0
-    while index < len(RC_stop)-2:
+    while index < len(RC_stop)-1:
+        temp=[]
         if RC_stop[index][4]==RC_stop[index+1][4]:
-            temp.extend(RC_stop[index])
+            temp.extend(RC_stop[index][0:5])
             RCedSP.append(temp)
-            temp=[]
         else:
-            if str2time(RC_stop[index][3],RC_stop[index+1][3])>=28*60:
-                temp.append([RC_stop[index][0:4]])
+            if str2time(RC_stop[index][3],RC_stop[index+1][3])>=20*60:
+                temp.append(RC_stop[index][0:5])
+                RCedSP.append(temp)
+                temp=[]
+                temp.append(RC_stop[index][0:5])
+                temp[3]=timedela(RC_stop[index][3])
+                RCedSP.append(temp)
+                print(index)
+            else:
+                temp.extend(RC_stop[index][0:5])
+                RCedSP.append(temp)
+        index+=1
+    last=[]
+    last.extend(RC_stop[len(RC_stop)-1][0:5])
+    RCedSP.append(last)
+    writeRCstop2addLabel(path,RCedSP)
+
+def timedela(str):
+    date_time1 = datetime.datetime.strptime(str,'%Y-%m-%d %H:%M:%S')
+    predate=date_time1+datetime.timedelta(seconds=-20)
+    s=predate.strftime('%Y-%m-%d %H:%M:%S')
+    return s
 
 def str2time(str1,str2):
     date_time1 = datetime.datetime.strptime(str1,'%Y-%m-%d %H:%M:%S')
@@ -33,22 +53,32 @@ def str2time(str1,str2):
     return (date_time2-date_time1).seconds
 
 def writeRCstop2addLabel(filename,SP):
-    filename=filename.replace('RC_stoppoint.txt','RCed_stoppoint.txt')
-    AfterSP=open(filename)
+    file_name=filename.replace('RC_stoppoint.txt','RCed_stoppoint.txt')
+    AfterSP=open(file_name,'w+')
+    for i in range(len(SP)):
+        AfterSP.write(SP[i][0])
+        AfterSP.write(',')
+        AfterSP.write(SP[i][1])
+        AfterSP.write(',')
+        AfterSP.write(SP[i][2])
+        AfterSP.write(',')
+        AfterSP.write(SP[i][3])
+        AfterSP.write(',')
+        AfterSP.write(SP[i][4])
+        if i != len(SP)-1:
+            AfterSP.write('\n')
+    AfterSP.close()
 
 
 
 if __name__=='__main__':
-    s1='2014-05-22 07:02:32'
-    s2='2014-05-22 08:48:32'
-    print(len(s1))
-    date_time1 = datetime.datetime.strptime(s1,'%Y-%m-%d %H:%M:%S')
-    date_time2 = datetime.datetime.strptime(s2,'%Y-%m-%d %H:%M:%S')
-    print (date_time2-date_time1).seconds
-    t=[['28.2296884085' '112.991525366' '16.0' '2014-05-22 23:57:32' '601']
- ['28.2296880819' '112.991550017' '16.0' '2014-05-22 23:57:42' '601']
- ['28.2297067478' '112.99157344' '24.0' '2014-05-22 23:57:52' '601']]
-    temp=[]
-    #temp.extend()
-    print(temp)
-    Read_RC_stoppoint('E:\\Research_Study\\GPS_Similar\\GPS_Get_PreProcesser\\5-23-2014\\RC_stoppoint.txt')
+    # s1='2014-05-22 07:02:32'
+    # s2='2014-05-22 08:48:32'
+    # print(len(s1))
+    # date_time1 = datetime.datetime.strptime(s1,'%Y-%m-%d %H:%M:%S')
+    # date_time2 = datetime.datetime.strptime(s2,'%Y-%m-%d %H:%M:%S')
+    # d=date_time1+datetime.timedelta(seconds=-3)
+    # print(d)
+
+    Read_RC_stoppoint('G:\\PostgraduatePROJECT\\GPS_Similar\\GPS_Get_PreProcesser\\5-23-2014\\RC_stoppoint.txt')
+    print('over')
