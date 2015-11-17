0c03177936b4494b49a2646f6acce24e64e678ab
wfing <757001119@qq.com>
2015-11-17 11:07:36 +0800

    label's LDA caiculate

--- LDA_calculate/LDA_Distance_calculate.py
+++ LDA_calculate/LDA_Distance_calculate.py
@@ -10,19 +10,19 @@ from gensim import corpora,models,similarities
 import math
 import datetime
 from label_add_time import GetSemanticGPSpath
-def create_model():
+def create_model(labelFileName):
     fullpath=GetSemanticGPSpath()
     train_set=[]
     onday=[]
     for file in fullpath:  #一个file 就是一天的数据路径，也就是一天的数据,外层一个for循环就是 写找完一天的记录
-        filename=file.replace('semanticGPS.txt','RClabelTime.txt')  #这里可以修改是用啥子文件名，就是说用是miss_label后的还之前的
-        tempdata=np.loadtxt(filename,dtype=str,delimiter=',',usecols=(0,1,3)) #label,starttime,continuetime
-        tempword=[]
-        for i in range(len(tempdata)):
-            datasstrip=tempdata[i][1].split(' ')
-            s=tempdata[i][0]+'_'+datasstrip[0]+'_'+time2hour(datasstrip[1])+'_'+str(int(tempdata[i][2])//(10*60))
-            tempword.append(s)
-        train_set.append(tempword)
+        filename=file.replace('semanticGPS.txt',labelFileName)  #这里可以修改是用啥子文件名，就是说用是miss_label后的还之前的
+        ondaydata=np.loadtxt(filename,dtype=str,delimiter=',',usecols=(0,1,3)) #label,starttime,continuetime
+        tempsentence=[]
+        for i in range(len(ondaydata)):
+            datasstrip=ondaydata[i][1].split(' ')
+            word=ondaydata[i][0]+'_'+datasstrip[0]+'_'+time2hour(datasstrip[1])+'_'+str(int(ondaydata[i][2])//(10*60))
+            tempsentence.append(word)
+        train_set.append(tempsentence)
     #print(train_set[0])
     dic = corpora.Dictionary(train_set)
     corpus = [dic.doc2bow(text) for text in train_set]
@@ -56,6 +56,39 @@ def find_max_LDA():
     fenmu=math.sqrt(np.dot(prob_predict,prob_predict))*math.sqrt(np.dot(prob_train,prob_train))
     temp_lda=fenzi/fenmu
     print(temp_lda)
+
+
+def test_find_max_LDA():
+    lda,dic,tfidf,trainset,onday=create_model('RClabelTime.txt')
+    groupA,groupB=getpeople()
+    for i in range(len(groupA)):
+        prob_predict=LDAInference(lda,dic,tfidf,day2sentence(groupA[i]))
+        prob_train=LDAInference(lda,dic,tfidf,day2sentence(groupB[i]))
+        fenzi=np.dot(prob_predict,prob_train)
+        fenmu=math.sqrt(np.dot(prob_predict,prob_predict))*math.sqrt(np.dot(prob_train,prob_train))
+        temp_lda=fenzi/fenmu
+        print temp_lda,(groupA[i]).split('\\')[4],(groupB[i]).split('\\')[4]
+
+def day2sentence(daypath):
+    UserData=[]
+    data=np.loadtxt(daypath,dtype=str,delimiter=',',usecols=(0,1,3))
+    for i in range(len(data)):
+            datasstrip=data[i][1].split(' ')
+            word=data[i][0]+'_'+datasstrip[0]+'_'+time2hour(datasstrip[1])+'_'+str(int(data[i][2])//(10*60))
+            UserData.append(word)
+    return UserData
+
+def  getpeople():
+     fullpath=GetSemanticGPSpath()
+     fullpath=[fullpath[i].replace('semanticGPS.txt','RClabelTime.txt') for i in range(len(fullpath))]
+     fullpath.sort()
+     print(fullpath)
+     groupA=fullpath[0:7]
+     groupB=fullpath[10:17]
+     return groupA,groupB
+
 if __name__=='__main__':
-    create_model()
+    #create_model('RClabelTime.txt')
     #find_max_LDA()
+    #getpeople()
+    test_find_max_LDA()
