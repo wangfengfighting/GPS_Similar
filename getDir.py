03f85f03e9ddcec086247a1838358ba5b8cd0773
wfing <757001119@qq.com>
2015-11-17 12:41:57 +0800

    label 缺失的添加

--- getDir.py
+++ getDir.py
@@ -1,8 +1,9 @@
+# coding: utf-8
 '''
 time 2015/10/13
 __author__ = 'WangFeng'
 '''
-# coding: utf-8
+
 import os
 class GetDirName:
     allFileNum=0
@@ -21,13 +22,30 @@ class GetDirName:
 
         return dirList
 
+def getUserFile():
+    import os
+    import os.path
+    rootdir =".\\starlog"                                 # 指明被遍历的文件夹
+    for parent,dirnames,filenames in os.walk(rootdir):
+        for dirname in dirnames:
+            print 'parent is:'+parent
+            print 'dirname is:'+dirname
+            print('full is')+os.path.join(parent,dirname)
 
+
+
+#     for parent,dirnames,filenames in os.walk(rootdir):    #三个参数：分别返回1.父目录 2.所有文件夹名字（不含路径） 3.所有文件名字
+# 　      for dirname in  dirnames:                       #输出文件夹信息
+#     　　    print "parent is:" + parent
+# 　　　　    print  "dirname is" + dirname
 
 
 if __name__=='__main__':
     a=GetDirName()
-    dir=a.printPath(".\\GPS_Get_PreProcesser")
+    dir=a.printPath(".\\starlog")
     dir.sort()
     print(len(dir))
     for i in dir :
-        print(i)
+        print(a.printPath('.\\starlog'+'\\'+i))
+        print('-------------------------------')
+    getUserFile()
