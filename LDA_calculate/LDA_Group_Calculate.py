034fbd147684fc978c124f9f395a4d44cf4d64e6
wfing <757001119@qq.com>
2015-11-18 11:16:06 +0800

    label's LDA caiculate

--- LDA_calculate/LDA_Group_Calculate.py
+++ LDA_calculate/LDA_Group_Calculate.py
@@ -10,6 +10,7 @@ from gensim import corpora,models,similarities
 import math
 from label_add_time import GetSemanticGPSpath
 import datetime
+import os
 def create_model(labelFileName='RClabelTime.txt'):
     FileName=[]
     from getDir import GetDirName
@@ -41,11 +42,68 @@ def create_model(labelFileName='RClabelTime.txt'):
     lda.save(".\\LDA_all_27\\SemanticLda"+str(24)+".txt")
     dic.save(".\\LDA_all_27\\SemanticDic"+str(24)+".txt")
     tfidf.save(".\\LDA_all_27\\SemanticTFIDF"+str(24)+".txt")
-    return  lda,dic,tfidf,train_set,[]
+
+    return  lda,dic,tfidf,train_set,AllUserFiles
+
+def LDAInference(lda,dic,tfidf,sentence):
+    sentence = dic.doc2bow(sentence)
+    sentence = tfidf[sentence]
+    prob = lda.inference([sentence])
+    prob = prob[0][0]
+    prob = prob / prob.sum()
+    return prob
+
+def find_max_LDA(lda,dic,tfidf,userGroup,labelFileName='RClabelTime.txt'):
+    # 两个for循环 就把group 内的用户组给循环完毕
+    final=[]
+    for UserNameIndex in range(len(userGroup)):
+        for UserNameIndexNext in range(UserNameIndex+1,len(userGroup)):
+            avg=[]
+            for dayindex in range   (       len(userGroup[UserNameIndex]) if len(userGroup[UserNameIndex])<=len(userGroup[UserNameIndexNext])
+            else  len(userGroup[UserNameIndexNext])   ):
+                prob_a=LDAInference(lda,dic,tfidf,day2sentence(userGroup[UserNameIndex][dayindex]+os.sep+labelFileName  )     )
+                prob_b=LDAInference(lda,dic,tfidf,day2sentence(userGroup[UserNameIndexNext][dayindex]+os.sep+labelFileName))
+                fenzi=np.dot(prob_a,prob_b)
+                fenmu=math.sqrt(np.dot(prob_a,prob_a))*math.sqrt(np.dot(prob_b,prob_b))
+                temp_lda=fenzi/fenmu
+                avg.append(temp_lda)
+            Average=sum(np.array(avg))/len(avg)
+            final.append( [ (userGroup[UserNameIndex][0]).split('\\')[4],(userGroup[UserNameIndexNext][0]).split('\\')[4],Average])
+                # prob_predict=LDAInference(lda,dic,tfidf)
+                # prob_train=LDAInference(lda,dic,tfidf,list(trainset[i][0] for i in range(6)))
+                # fenzi=np.dot(prob_predict,prob_train)
+                # fenmu=math.sqrt(np.dot(prob_predict,prob_predict))*math.sqrt(np.dot(prob_train,prob_train))
+                # temp_lda=fenzi/fenmu
+                # print(temp_lda)
+    print(final)
+    writeLDAans2file(final)
+
+def writeLDAans2file(seq):
+    file=open('LDAnetwork.txt','w+')
+    for ans in seq:
+        file.write(ans[0])
+        file.write(',')
+        file.write(ans[1])
+        file.write(',')
+        file.write(str(ans[2]))
+        file.write('\n')
+
+def day2sentence(daypath):
+    UserData=[]
+    data=np.loadtxt(daypath,dtype=str,delimiter=',',usecols=(0,1,3))
+    for i in range(len(data)):
+            datasstrip=data[i][1].split(' ')
+            word=data[i][0]+'_'+datasstrip[0]+'_'+time2hour(datasstrip[1])+'_'+str(int(data[i][2])//(10*60))
+            UserData.append(word)
+    return UserData
 
 def time2hour(strtime):
     d=datetime.datetime.strptime(strtime,'%H:%M:%S')
     return  str((d).hour)
 
+
+def main():
+    lda,dic,tfidf,trainset,other=create_model('RCedlabelTime.txt')
+    find_max_LDA(lda,dic,tfidf,other,'RCedlabelTime.txt')
 if __name__=='__main__':
-    create_model()
+    main()
---
+++ LDA_calculate/plot_LDA_network.py
@@ -0,0 +1,31 @@
+# coding: utf-8
+__author__ = 'WangFeng'
+from matplotlib import pylab as plt
+import math
+import numpy as np
+import networkx as nx
+from decimal import *
+data = np.loadtxt('LDAednetwork.txt',dtype=str,delimiter=',',usecols=(0,1,2))
+
+Dic={'u000':'yrs','u001':'xym','u002':'tzh','u003':'wf','u004':'lh','u005':'xym2'}
+
+print (data)
+G = nx.Graph()
+for i in range(0,len(data)):
+    if float(data[i][2])!=0.0:
+        G.add_node(Dic[data[i][0]])
+        G.add_node(Dic[(data[i][1])])
+
+for i in range(0,len(data)):
+        G.add_edge(Dic[(data[i][0])],Dic[(data[i][1])],weight=float((data[i][2]))*19.5)
+
+pos=nx.spring_layout(G,iterations=20)
+edgewidth=[]
+for (u,v,d) in G.edges(data=True):
+    #print G.get_edge_data(u,v).values()
+    edgewidth.append(round(G.get_edge_data(u,v).values()[0],2))
+
+nx.draw_networkx_edges(G,pos,width=edgewidth,with_labels=True,edge_color='k')
+#nx.draw_networkx_nodes(G,pos,with_labels=True,node_color='y')
+nx.draw(G,pos,with_labels=True, node_size=1000,node_color='y')
+plt.show()
