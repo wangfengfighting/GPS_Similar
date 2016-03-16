1f99d6d4e06036e385b490e9844549cd4ccf2174
wf <757001119@qq.com>
2016-03-16 20:12:19 +0800

    开始模式相似度的计算过程

--- Model_similary/caclu_model_similarity.py
+++ Model_similary/caclu_model_similarity.py
@@ -2,6 +2,7 @@
 __author__ = 'lym'
 from getDir import GetDirName
 import os
+import math
 def calculateModelSimilarity():
     getdir=GetDirName()
     Fulldirlist=[]
@@ -32,11 +33,32 @@ def frequence_mod_sim(user1,user2):
     user2filepath=parent_path+os.sep+"starlog"+os.sep+os.sep+user2+os.sep
     num_floder=len(user1Floder) if len(user1Floder)<=len(user2Floder) else len(user2Floder)
     for Floderindex in range(num_floder):
-        t_u1_file=user1filepath+user1Floder[Floderindex]+os.sep+
+        t_u1_file=user1filepath+user1Floder[Floderindex]+os.sep+'itemfrequence.txt'   #get use's itemfrequence data to caclulate simliarty
+        t_u2_file=user2filepath+user2Floder[Floderindex]+os.sep+'itemfrequence.txt'
+
+
     print(user1Floder)
     print(user2Floder)
 
+def itemEN(lista,listb):    #The calculated information entropy sequence
+    jiaoji=list (set(lista)&set(listb))
+    en=0
+    for item in jiaoji:
+        print (item)
+        print lista.count(item)
+        print listb.count(item)
+        print (len(lista)+len(listb))
+        pi = float( lista.count(item)+listb.count(item)) /  (len(lista)+len(listb))
+        print pi
+        if pi !=0.0:
+            en+= (    -pi*math.log(pi)       )
+        else:
+            en+=0.0
 
+    sim=en*len(jiaoji)/(len(lista)+len(listb))
+    #print(en)
+
+    return en,sim
 
 
 
